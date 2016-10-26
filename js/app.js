f543d8f512fe6d4dd800a231bb310c4e3e327e12
LizRuelas <liz.ruelas.borda@gmail.com>
2016-10-26 18:39:59 -0500

    agregando sobre mí

--- js/app.js
+++ js/app.js
@@ -8,5 +8,22 @@ $(document).ready(function(){
 			$('.menu').removeClass('menu-fixed');
 		}
 	});
- 
+
+	$('#maquinas').load(efectomaquina);
+
+	
 });
+function escribir(){
+		document.getElementById("maquinas").innerText= p.substr(0,num_c)+ "|";
+		num_c++;
+		if (num_c<=p.length){
+		setTimeout("escribir()", 100);
+		}
+	}
+	function efectomaquina() {
+		p="<Liz Ruelas Borda/>";
+		num_c=0;
+		escribir();
+	}
+
+	setTimeout("efectomaquina()",1000);
